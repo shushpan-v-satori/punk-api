b7633d21ce435f5c58c9291942a63643508bbda9
shushpan-v-satori <ianastasieva@gmail.com>
2022-10-24 17:07:06 -0400

    Updated the styles to autumn

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import CardList from "./components/CardList/CardList";
-import FiltersList from "./containers/FiltersList/FiltersList";
-import SearchBox from "./containers/SearchBox/SearchBox";
-
+import Nav from "./containers/Nav/Nav";
 
 const App = () => {
-  
   // fetch results from api
   const [beers, setBeers] = useState([]);
 
-  const url='https://api.punkapi.com/v2/beers?page=2&per_page=80'
+  const url = "https://api.punkapi.com/v2/beers?page=2&per_page=80";
 
   const getBeers = async () => {
     let res = await fetch(url);
     const data = await res.json();
     setBeers(data);
-  }
+  };
 
   useEffect(() => {
     getBeers();
-  }, [])
-
+  }, []);
 
   //working with search
 
   const [searchTerm, setSearchTerm] = useState("Search...");
 
-  const searchInputValue = (event) => {  
-   if ((event.target.value) === 'Search...') {
-    setSearchTerm('');
-   }
-  }
+  const searchInputValue = (event) => {
+    if (event.target.value === "Search...") {
+      setSearchTerm("");
+    }
+  };
 
   const handleInput = (event) => {
     console.log("in handle input");
@@ -43,49 +39,60 @@ const App = () => {
 
   // filtering out without images
 
-  const [beersHighAbv , setBeersHighAbv] =useState(false);
-  const [beersClassicRange , setBeersClassicRange] =useState(false);
-  const [beersAcidicPh4 , setBeersAcidicPh4] =useState(false);
+  const [beersHighAbv, setBeersHighAbv] = useState(false);
+  const [beersClassicRange, setBeersClassicRange] = useState(false);
+  const [beersAcidicPh4, setBeersAcidicPh4] = useState(false);
 
   const beersHighAbvFunction = (event) => {
+    console.log("in high abv");
     setBeersHighAbv(!beersHighAbv);
-  }
+  };
 
   const beersClassicRangeFunction = (event) => {
+    console.log("in classic range");
     setBeersClassicRange(!beersClassicRange);
-  }
+  };
 
-  const beersAcidicPh4Funciton = (event) => {
+  const beersAcidicPh4Function = (event) => {
+    console.log("in Ph4");
     setBeersAcidicPh4(!beersAcidicPh4);
-  }
+  };
 
   const filteredItems = beers.filter((beer) => {
     console.log("in filtered by fllter");
     const beerNameLower = beer.name.toLowerCase();
     const beerTagline = beer.tagline.toLowerCase();
-    const cleanSearchInput=searchTerm.toLowerCase();
-    return ((searchTerm!="Search...") ? (beerNameLower.includes(cleanSearchInput) ||
-    beerTagline.includes(cleanSearchInput)) : true)
-    &&(beersHighAbv ? beer.abv>6 : true)
-    &&(beersClassicRange ? Number(beer.first_brewed.substr(3, 6))<2010 : true)
-    &&(beersAcidicPh4 ? (parseFloat(beer.ph) < 4) : true)
-  }); 
+    const cleanSearchInput = searchTerm.toLowerCase();
+    return (
+      (searchTerm !== "Search..."
+        ? beerNameLower.includes(cleanSearchInput) ||
+          beerTagline.includes(cleanSearchInput)
+        : true) &&
+      (beersHighAbv ? beer.abv > 6 : true) &&
+      (beersClassicRange
+        ? Number(beer.first_brewed.substr(3, 6)) < 2010
+        : true) &&
+      (beersAcidicPh4 ? parseFloat(beer.ph) < 4 : true)
+    );
+  });
 
   return (
     <div className="punk-api">
-      <section className="punk-api__nav">
-        <SearchBox 
-        searchTerm={searchTerm} 
-        handleInput={handleInput} 
+      <Nav
+        searchTerm={searchTerm}
+        handleInput={handleInput}
         searchInputValue={searchInputValue}
-        />
-        <FiltersList
-          beersHighAbvFunction ={beersHighAbvFunction}
-          beersClassicRangeFunction = {beersClassicRangeFunction}
-          beersAcidicPh4Funciton = {beersAcidicPh4Funciton}
-        />
-      </section>
-     <CardList cardsList={filteredItems} />
+        beersHighAbvFunction={beersHighAbvFunction}
+        beersClassicRangeFunction={beersClassicRangeFunction}
+        beersAcidicPh4Function={beersAcidicPh4Function}
+      />
+      {filteredItems.length > 0 ? (
+        <CardList cardsList={filteredItems} />
+      ) : (
+        <p className="punk-api__empty-search">
+          Nothing to display, please modify your search
+        </p>
+      )}
     </div>
   );
 };
